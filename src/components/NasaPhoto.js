e0a5527a2bd8366c939efae66cc270ff396537d8
Jacob Grow <jacobagrow@gmail.com>
2020-11-02 11:16:24 -0700

    rendering photo

---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function NasaPhoto() {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    fetchPhoto();
+
+    async function fetchPhoto() {
+      const res = await fetch( `https://api.nasa.gov/planetary/apod?api_key=r8M5y9qD1L5zYtrXG1CkbgMi0gbXNEKuEOnXUw45`);
+      const data = await res.json();
+      setPhotoData(data);
+    }
+  }, []);
+  if (!photoData) return <div />;
+  return (
+<div>
+  <img 
+  src={photoData.url}
+  alt={photoData.title}
+  />
+</div>
+  )
+}

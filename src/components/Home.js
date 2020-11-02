3b0acf05f264f0bb5efdf51d616176302a53f0fe
Jacob Grow <jacobagrow@gmail.com>
2020-11-02 16:25:07 -0700

    added nav component to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from './Navbar'
 
 export default function Home() {
   return (
     <div>
-      <Link to="/nasaphoto">See into the stars!</Link>
+      <Navbar/>
     </div>
   )
 }

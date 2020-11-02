79f14bfeadc2bb28b7f1f70dd25b1cd1edcd4c81
Jacob Grow <jacobagrow@gmail.com>
2020-11-02 11:34:59 -0700

    hidden key

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+const apiKey = process.env.REACT_APP_NASA_KEY;
 export default function NasaPhoto() {
   const [photoData, setPhotoData] = useState(null);
 
@@ -8,7 +9,7 @@ export default function NasaPhoto() {
     fetchPhoto();
 
     async function fetchPhoto() {
-      const res = await fetch( `https://api.nasa.gov/planetary/apod?api_key=r8M5y9qD1L5zYtrXG1CkbgMi0gbXNEKuEOnXUw45`);
+      const res = await fetch( `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
       const data = await res.json();
       setPhotoData(data);
     }
@@ -16,10 +17,30 @@ export default function NasaPhoto() {
   if (!photoData) return <div />;
   return (
 <div>
+  {photoData.media_type == "image" ? (
+  
   <img 
   src={photoData.url}
   alt={photoData.title}
   />
+  ) : (
+    <iframe
+    title="space-video"
+    src={photoData.url}
+    frameBorder="0"
+    // gesture="media"
+    allow="encrypted-media"
+    allowFullScreen
+    className="photo"
+    />
+  )
+
+}
+  <div>
+    <h1>{photoData.title}</h1>
+    <p>{photoData.date}</p>
+    <p>{photoData.explanation}</p>
+  </div>
 </div>
   )
 }
